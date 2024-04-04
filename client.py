3f25e6b7d1327f916d592ca538a844234644285b
Reisenoff <gleb.bstr@yandex.ru>
2024-04-04 22:31:53 +0300

    new code

--- client.py
+++ client.py
@@ -1,27 +1,23 @@
 import socket
-import threading
 
-def handle_client(client_socket):
+def start_client(host, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+    print(f"Соединение с сервером {host}:{port}")
+
     while True:
-        data = client_socket.recv(1024)
-        if not data:
+        message = input("Введите сообщение (для выхода введите 'exit'): ")
+        if message == 'exit':
             break
-        client_socket.sendall(data)
+
+        client_socket.sendall(message.encode())
+        data = client_socket.recv(1024)
+        print("Получено от сервера:", data.decode())
+
+    print("Разрыв соединения с сервером")
     client_socket.close()
-
-def start_server(host, port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(5)
-    print(f"Сервер запущен. Ожидание подключений на {host}:{port}")
-
-    while True:
-        client_socket, client_address = server_socket.accept()
-        print(f"Подключение клиента {client_address}")
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-        client_thread.start()
 
 if __name__ == "__main__":
     host = input("Введите имя хоста (пустая строка для использования localhost): ") or 'localhost'
-    port = int(input("Введите номер порта: ") or '12345')
-    start_server(host, port)
+    port = int(input("Введите номер порта: "))
+    start_client(host, port)
