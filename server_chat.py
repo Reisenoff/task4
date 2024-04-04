1c979d9ebec76db7fab541219bd4b6cab5a6e904
Reisenoff <gleb.bstr@yandex.ru>
2024-04-04 22:47:30 +0300

    Сервер аутентификации: Этот сервер будет отвечать за аутентификацию пользователей и управление их именами.
    Сервер чата: Этот сервер будет отвечать за обмен сообщениями между подключенными клиентами.

---
+++ server_chat.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+def broadcast_message(message, sender_socket, clients):
+    for client in clients:
+        if client != sender_socket:
+            try:
+                client.sendall(message)
+            except:
+                clients.remove(client)
+
+def handle_client(client_socket, clients):
+    while True:
+        try:
+            message = client_socket.recv(1024)
+            if not message:
+                break
+            broadcast_message(message, client_socket, clients)
+        except:
+            break
+    client_socket.close()
+
+def start_chat_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+    print(f"Сервер чата запущен. Ожидание подключений на {host}:{port}")
+
+    clients = []
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Подключение клиента {client_address}")
+        clients.append(client_socket)
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, clients))
+        client_thread.start()
+
+# Запуск сервера чата
+if __name__ == "__main__":
+    chat_host = input("Введите имя хоста для сервера чата (пустая строка для использования localhost): ") or 'localhost'
+    chat_port = int(input("Введите номер порта для сервера чата: "))
+    start_chat_server(chat_host, chat_port)
